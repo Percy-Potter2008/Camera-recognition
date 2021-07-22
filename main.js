6fafc6e5621be969272e17677615d36f1480ea9b
Percy-Potter2008 <75735895+Percy-Potter2008@users.noreply.github.com>
2021-07-22 19:46:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 Webcam.attach('#camera' );
 camera=document.getElementById("camera")
 Webcam.set({
-  width : 350,
-  height:300,
+  width : 600,
+  height:600,
   image_format: 'png',
   png_quality: 90
 });
